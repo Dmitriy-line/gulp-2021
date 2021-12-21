26560a27cb7b6b85731394f4e7fcdafc09d71453
dmitriy-line <dpasternak011@gmail.com>
2021-12-21 23:01:54 +0300

    first commit

---
+++ #src/js/burger.js
@@ -0,0 +1,17 @@
+// burger
+
+let burger = document.querySelector('.burger');
+let nav = document.querySelector('.mobile__nav');
+let logo = document.querySelector('.hero-logo');
+let socials = document.querySelector('.hero-socials');
+
+const openMenu = () => {
+  burger.classList.toggle('active');
+  nav.classList.toggle('active');
+  logo.classList.toggle('active');
+  socials.classList.toggle('active');
+}
+
+burger.addEventListener('click', openMenu );
+
+// burger end
